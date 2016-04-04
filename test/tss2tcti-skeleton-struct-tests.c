9abe5c11f00b0ffe7aabfc5c4ec4a8963b39ff0d
Philip Tricca <philip.b.tricca@intel.com>
2016-04-04 13:24:45 -0700

    unit tests: Add test to verify version is set properly.
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/tss2tcti-skeleton-struct-tests.c
+++ test/tss2tcti-skeleton-struct-tests.c
@@ -51,11 +51,24 @@ tss2_tcti_struct_magic_test (void **state)
     assert_int_equal (tss2_tcti_context_magic (context), TSS2_TCTI_SKELETON_MAGIC);
 }
 
+/* Ensure that after initialization the 'version' value in the TCTI structure is
+ * the one that we expect.
+ */
+static void
+tss2_tcti_struct_version_test (void **state)
+{
+    TSS2_TCTI_CONTEXT *context = *state;
+    assert_int_equal (tss2_tcti_context_version (context), 1);
+}
+
 int
 main(int argc, char* argv[])
 {
     const UnitTest tests[] = {
         unit_test_setup_teardown (tss2_tcti_struct_magic_test,
+                                  tss2_tcti_struct_setup,
+                                  tss2_tcti_struct_teardown),
+        unit_test_setup_teardown (tss2_tcti_struct_version_test,
                                   tss2_tcti_struct_setup,
                                   tss2_tcti_struct_teardown),
     };
