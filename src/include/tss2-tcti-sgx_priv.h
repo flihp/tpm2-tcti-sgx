1e1eb887a4583b90322f8ff37d2b1dd0006a8dac
Philip Tricca <philip.b.tricca@intel.com>
2016-09-16 12:55:35 -0400

    Implement a simple state machine with two states to prevent invalid function calls.
    This state machine is intended to force the transmit and receive
    functions to be called in said order. Calling multiple transmits
    before calling receive is currently invalid (thought it may be
    possible?). Calling cancel when we're not waiting for a response
    makes no sense. Similarly getting a pollHandle only makes sense if
    we're waiting for a response. Finally the locality should only be
    set if we're not waiting for a resonse (causes ambiguity as to
    which locality the outstanding command will be executed with).
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/include/tss2-tcti-sgx_priv.h
+++ src/include/tss2-tcti-sgx_priv.h
@@ -7,6 +7,28 @@
 #define TSS2_TCTI_SGX_MAGIC 0x4e50bc1dcdb7623c
 #define TSS2_TCTI_SGX_ID(context) \
     ((TSS2_TCTI_CONTEXT_SGX*)context)->id
+#define TSS2_TCTI_SGX_STATE(context) \
+    ((TSS2_TCTI_CONTEXT_SGX*)context)->state
+
+/**
+ * There is a small state machine maintained by this TCTI. It is used to
+ * prevent callers from making nonsensical function calls like trying to
+ * call the receive function before calling transmit.
+ *
+ * After a successful call to the initialization function the context will
+ * be in the 'READY_TO_TRANSMIT' state. If the call to initialize fails for any
+ * reason, the state will be indeterminate.
+ * A successful call to tss2_tcti_transmit will set the state to
+ * READY_TO_RECEIVE. If the context is not in the READY_TO_TRANSMIT state
+ * when transmit is called then BAD_SEQUENCE will be returned.
+ * A successful call to tss2_tcti_receive will set the state to
+ * READY_TO_TRANSMIT. If the context is not in the READY_TO_RECEIVE state
+ * when receive is called then BAD_SEQUENCE will be returned.
+ */
+typedef enum {
+    READY_TO_RECEIVE,
+    READY_TO_TRANSMIT,
+} tss2_tcti_sgx_state_t;
 
 /* This is our private TCTI structure. We're required by the spec to have
  * the same structure as the non-opaque area defined by the
@@ -17,6 +39,7 @@
 typedef struct {
     TSS2_TCTI_CONTEXT_COMMON_V1 common;
     uint64_t                    id;
+    tss2_tcti_sgx_state_t       state;
 } TSS2_TCTI_CONTEXT_SGX;
 
 #endif /* TSS2_TCTI_SGX_PRIV_H */
--- src/tss2-tcti-sgx.c
+++ src/tss2-tcti-sgx.c
@@ -2,12 +2,28 @@
 #include "tss2-tcti-sgx_priv.h"
 #include "tss2_tcti_sgx_t.h"
 
+/**
+ * Simple accessor. Not sure it's any more simple than directly accessing
+ * the size field.
+ */
 size_t
 sizeof_sized_buf (const struct sized_buf *sbuf)
 {
     return sbuf->size;
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * transmit function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_transmit (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_REFERENCE when the context is NULL
+ * - TSS2_TCTI_RC_BAD_SEQUENCE  when the state machine is not in the
+ *   READY_TO_TRANSMIT state
+ * - TSS2_TCTI_RC_GENERAL_FAILURE when an SGX error occurs
+ */
 static TSS2_RC
 tss2_tcti_sgx_transmit (TSS2_TCTI_CONTEXT *tcti_context,
                         size_t             size,
@@ -16,6 +32,11 @@ tss2_tcti_sgx_transmit (TSS2_TCTI_CONTEXT *tcti_context,
     sized_buf buf = { size, command };
     sgx_status_t status;
     TSS2_RC retval;
+
+    if (tcti_context == NULL)
+        return TSS2_TCTI_RC_BAD_REFERENCE;
+    if (TSS2_TCTI_SGX_STATE (tcti_context) != READY_TO_TRANSMIT)
+        return TSS2_TCTI_RC_BAD_SEQUENCE;
 
     status = tss2_tcti_sgx_transmit_ocall (&retval,
                                            TSS2_TCTI_SGX_ID (tcti_context),
@@ -30,7 +51,19 @@ tss2_tcti_sgx_transmit (TSS2_TCTI_CONTEXT *tcti_context,
     else
         return TSS2_TCTI_RC_GENERAL_FAILURE;
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * receive function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_receive (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_REFERENCE when the context is NULL
+ * - TSS2_TCTI_RC_BAD_SEQUENCE  when the state machine is not in the
+ *   READY_TO_RECEIVE state
+ * - TSS2_TCTI_RC_GENERAL_FAILURE when an SGX error occurs
+ */
 static TSS2_RC
 tss2_tcti_sgx_receive (TSS2_TCTI_CONTEXT *tcti_context,
                        size_t            *size,
@@ -41,33 +74,89 @@ tss2_tcti_sgx_receive (TSS2_TCTI_CONTEXT *tcti_context,
     sgx_status_t status;
     TSS2_RC retval;
 
+    if (tcti_context == NULL)
+        return TSS2_TCTI_RC_BAD_REFERENCE;
+    if (TSS2_TCTI_SGX_STATE (tcti_context) != READY_TO_RECEIVE)
+        return TSS2_TCTI_RC_BAD_SEQUENCE;
+
     status = tss2_tcti_sgx_receive_ocall (&retval,
                                           TSS2_TCTI_SGX_ID (tcti_context),
                                           &buf,
                                           timeout);
-    return retval;
+    if (status == SGX_SUCCESS)
+        return retval;
+    else
+        return TSS2_TCTI_RC_GENERAL_FAILURE;
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * finalize function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_finalize (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns void so all errors are silently ignored (this is
+ * required by the TSS spec).
+ */
 static void
 tss2_tcti_sgx_finalize (TSS2_TCTI_CONTEXT *tcti_context)
 {
     sgx_status_t status;
 
+    if (tcti_context == NULL)
+        return;
+
     status = tss2_tcti_sgx_finalize_ocall (TSS2_TCTI_SGX_ID (tcti_context));
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * cancel function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_cancel (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_REFERENCE when the context is NULL
+ * - TSS2_TCTI_RC_BAD_SEQUENCE  when the state machine is not in the
+ *   READY_TO_RECEIVE state
+ * - TSS2_TCTI_RC_GENERAL_FAILURE when an SGX error occurs
+ */
 static TSS2_RC
 tss2_tcti_sgx_cancel (TSS2_TCTI_CONTEXT *tcti_context)
 {
     sgx_status_t status;
     TSS2_RC retval;
 
+    if (tcti_context == NULL)
+        return TSS2_TCTI_RC_BAD_REFERENCE;
+    if (TSS2_TCTI_SGX_STATE (tcti_context) != READY_TO_RECEIVE)
+        return TSS2_TCTI_RC_BAD_SEQUENCE;
+
     status = tss2_tcti_sgx_cancel_ocall (&retval,
                                          TSS2_TCTI_SGX_ID (tcti_context));
 
-    return retval;
+    if (status == SGX_SUCCESS)
+        return retval;
+    else
+        return TSS2_TCTI_RC_GENERAL_FAILURE;
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * getPollHandles function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_get_poll_handles (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_REFERENCE when the context is NULL
+ * - TSS2_TCTI_RC_BAD_SEQUENCE  when the state machine is not in the
+ *   READY_TO_RECEIVE state
+ * - TSS2_TCTI_RC_GENERAL_FAILURE when an SGX error occurs
+ * NOTE: Asynchronous IO across the enclave boundary is something I haven't
+ *       looked into seriously yet. The code on the other side of the
+ *       enclave boundary will likely reply with TSS2_TCTI_RC_NOT_IMPLEMENTED
+ *       while the ocall will still succeed.
+ */
 static TSS2_RC
 tss2_tcti_sgx_get_poll_handles (TSS2_TCTI_CONTEXT     *tcti_context,
                                 TSS2_TCTI_POLL_HANDLE *handles,
@@ -76,12 +165,32 @@ tss2_tcti_sgx_get_poll_handles (TSS2_TCTI_CONTEXT     *tcti_context,
     sgx_status_t status;
     TSS2_RC retval;
 
+    if (tcti_context == NULL)
+        return TSS2_TCTI_RC_BAD_REFERENCE;
+    if (TSS2_TCTI_SGX_STATE (tcti_context) != READY_TO_RECEIVE)
+        return TSS2_TCTI_RC_BAD_SEQUENCE;
+
     status =
         tss2_tcti_sgx_get_poll_handles_ocall (&retval,
                                               TSS2_TCTI_SGX_ID (tcti_context));
-    return retval;
+    if (status == SGX_SUCCESS)
+        return retval;
+    else
+        return TSS2_TCTI_RC_GENERAL_FAILURE;
 }
-
+/**
+ * This is the function that is hooked into the standard TSS2_TCTI_CONTEXT
+ * setLocality function pointer. This function will be invoked when:
+ * - TSS2_TCTI_CONTEXT object is initialized for the SGX TCTI
+ * - the tss2_tcti_set_locality (ctx ...) is invoked with said context
+ * It's also possible to invoke this function directly but that should be
+ * very rare.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_REFERENCE when the context is NULL
+ * - TSS2_TCTI_RC_BAD_SEQUENCE  when the state machine is not in the
+ *   READY_TO_TRANSMIT state
+ * - TSS2_TCTI_RC_GENERAL_FAILURE when an SGX error occurs
+ */
 static TSS2_RC
 tss2_tcti_sgx_set_locality (TSS2_TCTI_CONTEXT *tcti_context,
                             uint8_t            locality)
@@ -89,13 +198,43 @@ tss2_tcti_sgx_set_locality (TSS2_TCTI_CONTEXT *tcti_context,
     sgx_status_t status;
     TSS2_RC retval;
 
+    if (tcti_context == NULL)
+        return TSS2_TCTI_RC_BAD_REFERENCE;
+    if (TSS2_TCTI_SGX_STATE (tcti_context) != READY_TO_TRANSMIT)
+        return TSS2_TCTI_RC_BAD_SEQUENCE;
+
     status = tss2_tcti_sgx_set_locality_ocall (&retval,
                                                TSS2_TCTI_SGX_ID (tcti_context),
                                                locality);
 
-    return retval;
+    if (status == SGX_SUCCESS)
+        return retval;
+    else
+        return TSS2_TCTI_RC_GENERAL_FAILURE;
 }
-
+/**
+ * This is the initialization function for the SGX TCTI. It inplements a
+ * protocol similar to the TSS SAPI that enables the user to obtain the
+ * size of the context required by the TCTI using the following algorithm:
+ *   if   the 'tcti_context' parameter is NULL and the 'size' parameter is
+ *        not
+ *   then the 'size' parameter will be set to the required memory size fot
+ *        the context object.
+ *
+ * When called with a non NULL 'tcti_context' this function sets up all of the
+ * necessary function pointers for the generic TCTI structure, calls the
+ * initialization ocall to get an ID from the resource manager / tpm2
+ * access broker outside the enclave and then sets the state to
+ * READY_TO_TRANSMIT.
+ * This function returns:
+ * - TSS2_TCTI_RC_BAD_VALUE: when the 'tcti_context' parameter is NULL or
+ *   when the 'size' parameter is NULL.
+ * - TSS2_TCTI_RC_GENERAL_FAILURE: when the init ocall fails for any reason
+ * - TSS2_RC_SUCCESS: when initialization completes successfully or when
+ *   the caller passes in a null context object and a non null size. In this
+ *   case the required size for the context object is returned through the
+ *   'size' parameter.
+ */
 TSS2_RC
 tss2_tcti_sgx_init (TSS2_TCTI_CONTEXT *tcti_context,
                     size_t            *size)
@@ -118,6 +257,12 @@ tss2_tcti_sgx_init (TSS2_TCTI_CONTEXT *tcti_context,
     TSS2_TCTI_SET_LOCALITY (tcti_context) = tss2_tcti_sgx_set_locality;
 
     status = tss2_tcti_sgx_init_ocall (&TSS2_TCTI_SGX_ID (tcti_context));
-
+    if (status != SGX_SUCCESS)
+        return TSS2_TCTI_RC_GENERAL_FAILURE;
+    /**
+     * Only set state to READY_TO_TRANSMIT after ocall to initialize
+     * connection completes successfully
+     */
+    TSS2_TCTI_SGX_STATE (tcti_context) = READY_TO_TRANSMIT;
     return TSS2_RC_SUCCESS;
 }
