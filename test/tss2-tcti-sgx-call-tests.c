a0fbe19430faf936fc0a1e59f044ef81a9edb5b7
Philip Tricca <philip.b.tricca@intel.com>
2016-09-22 11:34:40 -0400

    Stop using the tss2_tcti_* wrappers.
    These macros do a few sanity checks that we don't want to mess with.
    Also it's code that's not from this project so we shouldn't be testing
    it.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/tss2-tcti-sgx-call-tests.c
+++ test/tss2-tcti-sgx-call-tests.c
@@ -26,7 +26,7 @@ tss2_tcti_call_transmit_success_test (void **state)
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, TSS2_RC_SUCCESS);
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, SGX_SUCCESS);
 
-    assert_int_equal (tss2_tcti_transmit (context, size, &command),
+    assert_int_equal (tss2_tcti_sgx_transmit (context, size, &command),
                       TSS2_RC_SUCCESS);
 }
 /**
@@ -45,7 +45,7 @@ tss2_tcti_call_transmit_sgx_fail_test (void **state)
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, TSS2_RC_SUCCESS);
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, SGX_ERROR_OUT_OF_EPC);
 
-    assert_int_equal (tss2_tcti_transmit (context, size, &command),
+    assert_int_equal (tss2_tcti_sgx_transmit (context, size, &command),
                       TSS2_TCTI_RC_GENERAL_FAILURE);
 }
 static void
@@ -58,7 +58,7 @@ tss2_tcti_call_transmit_tcti_fail_test (void **state)
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, TSS2_SYS_RC_BAD_REFERENCE);
     will_return (__wrap_tss2_tcti_sgx_transmit_ocall, SGX_SUCCESS);
 
-    assert_int_equal (tss2_tcti_transmit (context, size, &command),
+    assert_int_equal (tss2_tcti_sgx_transmit (context, size, &command),
                       TSS2_SYS_RC_BAD_REFERENCE);
 }
 static void
@@ -69,7 +69,7 @@ tss2_tcti_call_receive_test (void **state)
     uint8_t response;
     uint32_t timeout;
 
-    assert_int_equal (tss2_tcti_receive (context, &size, &response, timeout),
+    assert_int_equal (tss2_tcti_sgx_receive (context, &size, &response, timeout),
                       TSS2_TCTI_RC_NOT_IMPLEMENTED);
 }
 static void
@@ -77,7 +77,7 @@ tss2_tcti_call_cancel_test (void **state)
 {
     TSS2_TCTI_CONTEXT *context = *state;
 
-    assert_int_equal (tss2_tcti_cancel (context),
+    assert_int_equal (tss2_tcti_sgx_cancel (context),
                       TSS2_TCTI_RC_NOT_IMPLEMENTED);
 }
 static void
@@ -86,9 +86,10 @@ tss2_tcti_call_get_poll_handles_test (void **state)
     TSS2_TCTI_CONTEXT *context = *state;
     TSS2_TCTI_POLL_HANDLE handles;
     size_t num_handles;
+    TSS2_RC rc;
 
-    assert_int_equal (tss2_tcti_get_poll_handles (context, &handles, &num_handles),
-                      TSS2_TCTI_RC_NOT_IMPLEMENTED);
+    rc = tss2_tcti_sgx_get_poll_handles (context, &handles, &num_handles);
+    assert_int_equal (rc, TSS2_TCTI_RC_NOT_IMPLEMENTED);
 }
 static void
 tss2_tcti_call_set_locality_test (void **state)
@@ -96,7 +97,7 @@ tss2_tcti_call_set_locality_test (void **state)
     TSS2_TCTI_CONTEXT *context = *state;
     uint8_t locality;
 
-    assert_int_equal (tss2_tcti_set_locality (context, locality),
+    assert_int_equal (tss2_tcti_sgx_set_locality (context, locality),
                       TSS2_TCTI_RC_NOT_IMPLEMENTED);
 }
 int
