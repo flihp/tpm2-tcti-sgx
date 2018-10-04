db32ad21e2394a61305df089b4652cba1b408ef2
Philip Tricca <philip.b.tricca@intel.com>
2018-10-04 14:33:17 -0700

    test: Add test for 'open' failure path.
    This requires mocking the 'open' function call which must be done with
    care. The tools we use to collect code coverage metrics for our tests
    write to files and so they need to be able to call 'open' w/o us getting
    in the way. Our solution to this is to only mock calls to open the file
    /dev/urandom which the mgr module uses to get random ID values.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/tcti-sgx-mgr-init-tests.c
+++ test/tcti-sgx-mgr-init-tests.c
@@ -2,8 +2,10 @@
  * Copyright 2018, Intel Corporation
  * SPDX-License-Identifier: BSD-2-Clause
  */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <setjmp.h>
 #include <cmocka.h>
@@ -35,6 +37,37 @@ __wrap_calloc (size_t nmemb,
     }
 }
 
+int
+__real_open (const char *pathname,
+             int flags);
+int
+__wrap_open (const char *pathname,
+             int flags)
+{
+     if (strcmp ("/dev/urandom", pathname) == 0) {
+         printf ("%s: mock open\n", __func__);
+         errno = mock_type (int);
+         return mock_type (int);
+     } else {
+         printf ("%s: real open\n", __func__);
+         return __real_open (pathname, flags);
+     }
+}
+
+TSS2_TCTI_CONTEXT*
+callback (void *user_data)
+{
+    return NULL;
+}
+
+static int
+tcti_sgx_mgr_init_setup (void **state)
+{
+    will_return (__wrap_calloc, passthrough);
+    *state = tcti_sgx_mgr_init (callback, NULL);
+    return 0;
+}
+
 static int
 tcti_sgx_mgr_init_teardown (void **state)
 {
@@ -47,12 +80,6 @@ tcti_sgx_mgr_init_null_callback (void **state)
 {
     tcti_sgx_mgr_t *mgr = tcti_sgx_mgr_init (NULL, NULL);
     assert_null (mgr);
-}
-
-TSS2_TCTI_CONTEXT*
-callback (void *user_data)
-{
-    return NULL;
 }
 
 static void
@@ -80,6 +107,16 @@ tcti_sgx_mgr_init_ocall_no_init (void **state)
     uint64_t id = tcti_sgx_init_ocall ();
     assert_int_equal (id, 0);
 }
+
+static void
+tcti_sgx_mgr_init_ocall_open_fail (void **state)
+{
+    will_return (__wrap_open, EACCES);
+    will_return (__wrap_open, -1);
+    uint64_t id = tcti_sgx_init_ocall ();
+    assert_int_equal (id, 0);
+}
+
 int
 main (void)
 {
@@ -91,6 +128,9 @@ main (void)
         cmocka_unit_test_teardown (tcti_sgx_mgr_init_null_data,
                                    tcti_sgx_mgr_init_teardown),
         cmocka_unit_test (tcti_sgx_mgr_init_ocall_no_init),
+        cmocka_unit_test_setup_teardown (tcti_sgx_mgr_init_ocall_open_fail,
+                                         tcti_sgx_mgr_init_setup,
+                                         tcti_sgx_mgr_init_teardown),
     };
 
     return cmocka_run_group_tests (tests, NULL, NULL);
