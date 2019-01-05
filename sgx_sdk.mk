42bf8753bb5d72a90d550213fd6844ad064bd8b8
Philip Tricca <Philip Tricca philip.b.tricca@intel.com>
2019-01-05 13:34:08 -0800

    build: Use $(builddir) as destination for generated SGX files.
    This is necessary for VPATH builds to work properly. It must be possible
    for $(srcdir) to be readonly.
    
    Signed-off-by: Philip Tricca <Philip Tricca philip.b.tricca@intel.com>

--- sgx_sdk.mk
+++ sgx_sdk.mk
@@ -6,8 +6,8 @@ ENCLAVE_SEARCH_PATH = --search-path $(srcdir)/src \
     --search-path $(srcdir)/src/include --search-path $(SGX_INCLUDE_DIR)
 
 %_u.h %_u.c : %.edl
-	$(SGX_EDGER8R_BIN) --untrusted $(ENCLAVE_SEARCH_PATH) --untrusted-dir $(dir $^) $^
+	$(SGX_EDGER8R_BIN) --untrusted $(ENCLAVE_SEARCH_PATH) --untrusted-dir ${subst $(srcdir),$(builddir),$(dir $^)} $^
 
 %_t.h %_t.c : %.edl
-	$(SGX_EDGER8R_BIN) --trusted $(ENCLAVE_SEARCH_PATH) --trusted-dir $(dir $^) $^
+	$(SGX_EDGER8R_BIN) --trusted $(ENCLAVE_SEARCH_PATH) --trusted-dir ${subst $(srcdir),$(builddir),$(dir $^)} $^
 
