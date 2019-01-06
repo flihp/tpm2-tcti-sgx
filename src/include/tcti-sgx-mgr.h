9ed5435fa5716f9f575e5117c22d6aac690be711
Philip Tricca <Philip Tricca philip.b.tricca@intel.com>
2019-01-05 16:28:10 -0800

    tcti-sgx-mgr: remove unnecessary declarations for _ocall functions
    These functions don't make sense in the header for the tcti-sgx-mgr
    library even though it provides these symbols. Their declarations will
    be generated by the edger8r tool when processing the tss2_tcti_sgx.edl
    file.
    
    Signed-off-by: Philip Tricca <Philip Tricca philip.b.tricca@intel.com>

--- src/include/tcti-sgx-mgr.h
+++ src/include/tcti-sgx-mgr.h
@@ -15,20 +15,5 @@ typedef TSS2_TCTI_CONTEXT* (*downstream_tcti_init_cb) (void *user_data);
 int tcti_sgx_mgr_init (downstream_tcti_init_cb callback,
                        void *user_data);
 void tcti_sgx_mgr_finalize (void);
-uint64_t tcti_sgx_init_ocall ();
-TSS2_RC tcti_sgx_transmit_ocall (uint64_t id,
-                                 size_t size,
-                                 const uint8_t *command);
-TSS2_RC tcti_sgx_receive_ocall (uint64_t id,
-                                size_t size,
-                                uint8_t *response,
-                                uint32_t timeout);
-void tcti_sgx_finalize_ocall (uint64_t id);
-TSS2_RC tcti_sgx_cancel_ocall (uint64_t id);
-TSS2_RC tcti_sgx_get_poll_handles_ocall (uint64_t id,
-                                         TSS2_TCTI_POLL_HANDLE *handles,
-                                         size_t *num_handles);
-TSS2_RC tcti_sgx_set_locality_ocall (uint64_t id,
-                                     uint8_t locality);
 
 #endif
